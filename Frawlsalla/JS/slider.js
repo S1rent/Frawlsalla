57d070284cb02428e947250415abb02ce7537790
Philip indra prayitno <philip.prayitno@gmail.com>
2021-05-11 23:56:59 +0700

    feat: home slider

---
+++ Frawlsalla/JS/slider.js
@@ -0,0 +1,39 @@
+$(function() {
+    var $slider = $('#content-image-slider');
+    var $slideContainer = $('.content-slides', $slider);
+    var $slides = $('.content-slide', $slider);
+    var currentSlide = 1;
+    var indicators = document.getElementsByClassName("dot");
+    var contentWidth = 1024;
+
+    var interval;
+
+    function startSlider() {
+        if(currentSlide === 1){
+            indicators[0].className += " active";
+        }
+        interval = setInterval(function() {
+            $slideContainer.animate(
+                    { 'margin-left': '-=' + contentWidth }, 
+                    1000, 
+                    function() {
+                        for (i = 0; i < indicators.length; i++) {
+                            indicators[i].className = indicators[i].className.replace(" active", "");
+                        }
+                        indicators[currentSlide].className += " active";
+                        if (++currentSlide === $slides.length) {
+                            setTimeout(function () {
+                                currentSlide = 1;
+                                $slideContainer.css('margin-left', 0);
+                                for (i = 0; i < indicators.length; i++) {
+                                    indicators[i].className = indicators[i].className.replace(" active", "");
+                                }
+                                indicators[0].className += " active";
+                            }, 1000);
+                        }
+                    }
+                );
+        }, 3000);
+    }
+    startSlider();
+});
