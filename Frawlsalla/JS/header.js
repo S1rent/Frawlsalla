404786891f84843245fdae66da2c115b5cfe4e42
Philip indra prayitno <philip.prayitno@gmail.com>
2021-05-19 14:31:16 +0700

    fix: bad transition header dropdown bug

--- Frawlsalla/JS/header.js
+++ Frawlsalla/JS/header.js
@@ -3,9 +3,11 @@ var isToggled = false;
 function toggleMenu() {
   var navbar = document.getElementById("header-navbar-menu-list");
   if(!isToggled) {
+    navbar.style.opacity = '1';
     navbar.style.visibility = 'visible';
     navbar.style.height = 'auto';
   } else {
+    navbar.style.opacity = '0';
     navbar.style.visibility = 'hidden';
     navbar.style.height = '0';
   }
